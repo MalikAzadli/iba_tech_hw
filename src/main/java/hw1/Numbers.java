6f2a8ebbcf8d2b23733af26555a53cabc6a9a8a9
MalikIbadov <malikibadov97@gmail.com>
2019-10-18 16:33:20 +0400

    --hw1 and hw2 added

--- src/main/java/hw1/Numbers.java
+++ src/main/java/hw1/Numbers.java
@@ -1,7 +1,48 @@
+
 package hw1;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Random;
+import java.util.Scanner;
 
 public class Numbers {
     public static void main(String[] args) {
-        System.out.println("Hell World");
+        Random rand = new Random();
+        Scanner scan = new Scanner(System.in);
+        //Array to store all guessed numbers
+        ArrayList<Integer> user_numbers = new ArrayList<>();
+        //program choose random number from range of [0-100]
+        int chosenNumber = rand.nextInt(101);
+
+        System.out.print("Please, enter your name.\nName: ");
+        String name = scan.nextLine(); //scan player name
+        System.out.println("Let the game begin!");
+
+        int player_num;
+
+        do {
+            System.out.print("Enter your guess: ");
+            //checks the validity of the input
+            while(!scan.hasNextInt()) {
+                System.out.print("This is not a number\nPlease, enter a number: ");
+                scan.next();
+            }
+            //scan user's guess
+            player_num = scan.nextInt();
+            //add number to the array to keep record of all guesses
+            user_numbers.add(player_num);
+            //checks whether number is bigger or smaller than hidden number
+            if(player_num > chosenNumber) {
+                System.out.println("Your number is too big. Please, try again.");
+            } else if( player_num < chosenNumber){
+                System.out.println("Your number is too small. Please, try again.");
+            }
+
+        } while (player_num != chosenNumber);
+
+        System.out.printf("Congratulations, %s!\n", name);
+        Collections.sort(user_numbers); //sorting numbers that was guessed by player
+        System.out.println("Your numbers: " + user_numbers);
     }
 }
---
+++ src/main/java/hw2/AreaShooting.java
@@ -0,0 +1,122 @@
+package hw2;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class AreaShooting {
+
+    private static Scanner scan = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        Random rand = new Random();
+        String[][] battleField = createField(5,5);
+        //variables to keep record of target
+        int tarLine = rand.nextInt(5)+1;
+        int tarBar = rand.nextInt(5)+1;
+        //number of shoots that have been completed
+        int shoot_number = 0;
+
+        System.out.println("All set. Get ready to rumble!");
+
+        //variables to keep record of current shoot location
+        int chosenLine, chosenBar;
+
+        do {
+            System.out.print("Enter line number to fire: ");
+            chosenLine = validationCheck();
+
+            System.out.print("Enter bar number to fire: ");
+            chosenBar = validationCheck();
+
+            shootLocation(chosenLine,chosenBar,battleField);
+            shoot_number++;
+        } while (tarLine != chosenLine || tarBar != chosenBar);
+        System.out.println("You have won!");
+        //after game finished, target location is marked with 'x'
+        battleField[tarLine][tarBar] = " x |";
+
+        printField(battleField);
+        System.out.printf("You have defeated the enemy after %d shoots", shoot_number);
+    }
+
+    /**
+     * Checks whether the number that is chosen by user is valid
+     * in terms of the size of the field.
+     * @return int will be returned if input is valid
+     */
+    private static int validationCheck() {
+        int chosenLocation = isNumber();
+        //checks validity of the number
+        while(chosenLocation > 5 || chosenLocation < 1){
+            System.out.print("This is not a valid number.\nEnter new number between 1 and 5: ");
+            chosenLocation = isNumber();
+        }
+        return chosenLocation;
+    }
+
+    /**
+     * Checks whether input passed by user is number (integer) or not
+     * @return int
+     */
+    private static int isNumber(){
+        int number;
+        //asks for a new input if input passed before is not an integer
+        while(!scan.hasNextInt()){
+            System.out.print("This is not a number. Please, enter a number: ");
+            scan.next();
+        }
+        number=scan.nextInt();
+        return number;
+    }
+
+    /**
+     * Creates field according to the numbers have passed. Extra line and bar
+     * will be added to show the location of the respective rows and columns
+     * @param height height of the field
+     * @param width width of the field
+     * @return Multidimensional array of String will be returned
+     */
+    private static String[][] createField(int height, int width){
+        String[][] arr = new String[height+1][width+1];
+        for(int i = 0; i < arr.length; i++ ){
+            for(int z = 0; z < arr[i].length; z++) {
+                if(i==0) {
+                    //respective numbers will be added through first bar
+                    arr[i][z] = " " + z + " |";
+                } else if(z==0) {
+                    //respective numbers will be added through first line
+                    arr[i][z] = " " + i + " |";
+                } else {
+                    arr[i][z] = " - |";
+                }
+            }
+        }
+        return arr;
+    }
+
+    /**
+     * Prints the passed multidimensional Array of String
+     * @param arr Array that will be printed in console
+     */
+    private static void printField(String[][] arr){
+        //current values of all cells will be printed
+        for (String[] strings : arr) {
+            for (String string : strings) {
+                System.out.print(string);
+            }
+            System.out.println();
+        }
+    }
+
+    /**
+     * Specific location of the field will be shot according to the passed arguments.
+     * @param line row number
+     * @param bar column number
+     * @param arr Array which will be modified
+     */
+    private static void shootLocation(int line, int bar, String[][] arr){
+        //cell that has been shot will be marked with asterisk
+        arr[line][bar]=" * |";
+        printField(arr);
+    }
+}
