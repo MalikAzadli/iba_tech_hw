382d40d001cae9e33d1f6c7218cf381654230821
MalikIbadov <malikibadov97@gmail.com>
2019-10-19 23:02:44 +0400

    --hw5 some parts added

--- src/main/java/hw/hw5/Family.java
+++ src/main/java/hw/hw5/Family.java
@@ -1,4 +1,81 @@
 package hw.hw5;
 
+import org.graalvm.compiler.hotspot.meta.HotSpotUnsafeSubstitutions;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Family {
+
+    private final Human mother;
+    private final Human father;
+    private Pet pet;
+    private ArrayList<Human> children;
+
+    public Family(Human father, Human mother){
+        this.father = father;
+        this.mother = mother;
+        this.children = new ArrayList<>();
+    }
+
+    public Human getMother() {
+        return mother;
+    }
+
+    public Human getFather() {
+        return father;
+    }
+
+    public Pet getPet() {
+        return pet;
+    }
+
+    public ArrayList<Human> getChildren() {
+        return children;
+    }
+
+    public void setPet(Pet pet) {
+        this.pet = pet;
+    }
+
+    public void setChildren(ArrayList<Human> children) {
+        this.children = children;
+    }
+
+    public void addChild(Human child){
+        this.children.add(child);
+        child.setFamily(this);
+    }
+
+    public boolean deleteChild(Human child){
+        int index = children.indexOf(child);
+        Human human = children.remove(index);
+        if(children.indexOf(index) == -1) {
+            human.setFamily(null);
+            return true;
+        }
+        return false;
+    }
+
+    public int countFamily(){
+        return children.size() + 2;
+    }
+
+    @Override
+    public boolean equals(Object that) {
+        if(this == null) return false;
+        if(this == that) return true;
+        if(!(that instanceof Family)) return false;
+
+        Family family = (Family) that;
+        if(this.mother.equals(family.mother) && this.father.equals(family.father)) return true;
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        String s = String.format("Family{father=%s, mother=%s, children=%s",
+                father.getFullName(), mother.getFullName(), children);
+        return s;
+    }
 }
---
+++ src/main/java/hw/hw5/Human.java
@@ -0,0 +1,94 @@
+package hw.hw5;
+
+import java.util.Arrays;
+import java.util.Objects;
+
+public class Human {
+    //class variables
+    private String name;
+    private String surname;
+    private int year;
+    private Pet pet;
+    private Family family;
+    private String[][] schedule;
+
+    public Human(String name, String surname, int year, Family family) {
+        this.name = name;
+        this.surname = surname;
+        this.year = year;
+        this.family = family;
+    }
+
+    public Human(String name, String surname, int year, Pet pet, Family family, String[][] schedule) {
+        this.name = name;
+        this.surname = surname;
+        this.year = year;
+        this.pet = pet;
+        this.family = family;
+        this.schedule = schedule;
+    }
+
+    public void greetPet(){
+        System.out.println("Hello, " + pet.getNickname());
+    }
+
+    public void describePet(){
+        String slyNess = pet.getAge() > 50 ? "very sly" : "almost not sly";
+        System.out.printf("I have a %s, he is %d years old, he is %s\n", pet.getSpecies(), pet.getAge(), slyNess);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public String getFullName() { return name+" "+surname; }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public void setPet(Pet pet) {
+        this.pet = pet;
+    }
+
+    public void setSchedule(String[][] schedule) {
+        this.schedule = schedule;
+    }
+
+    public void setFamily(Family family){
+        this.family = family;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Human human = (Human) o;
+        return name.equals(human.name) &&
+                surname.equals(human.surname) &&
+                Objects.equals(family, human.family);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, surname, family);
+    }
+
+    @Override
+    public String toString() {
+        String s = String.format("Human{name='%s', surname='%s', year=%d, iq=1, " +
+                        "schedule=%s}", name, surname, year, Arrays.toString(schedule));
+
+        return s;
+    }
+
+
+}
