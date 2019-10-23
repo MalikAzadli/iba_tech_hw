200da4fcada3bf87402640e313eab0f8290cfce7
MalikIbadov <malikibadov97@gmail.com>
2019-10-24 00:26:18 +0400

    --minor changes on hw6

--- src/main/java/hw/hw6/Main.java
+++ src/main/java/hw/hw6/Main.java
@@ -2,6 +2,11 @@ package hw.hw6;
 
 public class Main {
     public static void main(String[] args) {
-        DayofWeek.MONDAY.name();
+        String[][] schedule = new String[7][2];
+        schedule[0][0] = DayofWeek.SUNDAY.name();
+        schedule[0][1] = "just some routine stuff";
+        for(int i = 0; i < 1000000; i++){
+            Human human = new Human("James", "Stewart", 1900, null);
+        }
     }
 }
