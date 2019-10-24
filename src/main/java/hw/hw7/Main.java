c461eb82706785b65f88e620d9efadc9cd09c87a
MalikIbadov <malikibadov97@gmail.com>
2019-10-24 18:44:41 +0400

    --kinda test

--- src/main/java/hw/hw7/Main.java
+++ src/main/java/hw/hw7/Main.java
@@ -5,5 +5,7 @@ public class Main {
 
         Pet roboCat = new RoboCat(Species.ROBOCAT, "Janie");
         RoboCat cat = new RoboCat(Species.ROBOCAT, "maxie");
+        Man max = new Man("M", "S", 1234, null);
+
     }
 }
