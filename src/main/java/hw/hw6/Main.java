459327e9d3e81262307d00b8ccdbe012775b4da2
MalikIbadov <malikibadov97@gmail.com>
2019-10-25 17:20:15 +0400

    --finalize method used

--- src/main/java/hw/hw6/Main.java
+++ src/main/java/hw/hw6/Main.java
@@ -1,12 +1,16 @@
 package hw.hw6;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Throwable {
         String[][] schedule = new String[7][2];
         schedule[0][0] = DayOfWeek.SUNDAY.name();
         schedule[0][1] = "just some routine stuff";
-        for(int i = 0; i < 1000000; i++){
+        int count = 0;
+        for(int i = 0; i < 1000; i++){
+            count++;
+
             Human human = new Human("James", "Stewart", 1900, null);
+            if(count > 990) human.finalize();
         }
     }
 }
